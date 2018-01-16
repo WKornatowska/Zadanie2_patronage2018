e63920a850c6dc58f5ca32b627183893b1e61a4a
WKornatowska <kornatowskawiktoria@gmail.com>
2018-01-16 17:54:12 +0100

    Zadanie2 - update

--- Zadanie2/src/test/java/com/intive/patronage/tests/SearchGoogle.java
+++ Zadanie2/src/test/java/com/intive/patronage/tests/SearchGoogle.java
@@ -1,27 +1,16 @@
 package com.intive.patronage.tests;
-
-import com.google.common.util.concurrent.FluentFuture;
-import com.sun.deploy.util.Waiter;
-import com.sun.javafx.jmx.HighlightRegion;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import javax.swing.text.Highlighter;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Logger;
 
 
 public class SearchGoogle {
     public SearchGoogle() {
-        System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver\\chromedriver.exe"); //path to googledriver
+        System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver\\chromedriver.exe");
     }
 
     @Test
@@ -32,8 +21,7 @@ public class SearchGoogle {
         String matchUrl = "https://www.intive.com";
 
         driver.get("http://google.com");
-        driver.findElement(By.name("q")).sendKeys(searchTerm);
-        driver.findElement(By.name("btnK")).click();
+        driver.findElement(By.name("q")).sendKeys(searchTerm,Keys.ENTER);
 
         int rank = 0;
         boolean foundMatch = false;
@@ -44,11 +32,12 @@ public class SearchGoogle {
             if (link.contains(matchUrl)) {
                 System.out.println("Website position in search results: " + rank);
                 foundMatch = true;
-                assert(rank == 1);
                 break;
             }
         }
-        assert(foundMatch==true);
+        driver.close();
+        assert(foundMatch = true);
+        assert(rank == 1);
         }
     }
 
